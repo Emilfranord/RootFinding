9cf109c6054c85f71361e720be52d2514a2345fb
Emil <emilfranord@gmail.com>
2021-03-09 11:49:21 +0100

    N/A

--- Find.java
+++ Find.java
@@ -348,7 +348,10 @@ class improvedHouseholder implements ItMe{// Nazeer, W.: A new Householder metho
 		Double fy  = f.evaluate(y);
 		Double fpy = fp.evaluate(y);
 		
-		return y - ((fy)/(fpy)) * (1- (fpy*fpx*fy - fpx*fpx*fx)/(2*fpy*fpy*fx));
+		//return y - ((fy)/(fpy)) * (1- (fpy*fpx*fy - fpx*fpx*fx)/(2*fpy*fpy*fx));
+		return y - ((fy)/(fpy)) * (1- (fpy*fpx*fy - fp.evaluate(fp.evaluate(x))*fx)/(2* fp.evaluate(fp.evaluate(y)) *fx));
+		// The second method likely more correct, but i has not been used in the tests.
+		// It depends on the syntax f'^2(x), and if that is f'(f'(x)) or f'(x)* f'(x).
 	}
 	
 	public String toString(){
